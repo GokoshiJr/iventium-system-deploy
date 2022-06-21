e3d4c151e5431122f957b08d15060c56de3c4285
Julio Gonzalez <julioagonzalez18@gmail.com>
2022-06-21 18:18:20 -0400

    feat: Add build folder and update config

--- index.js
+++ index.js
@@ -7,29 +7,17 @@ const { appConfig } = require('./config');
 const app = express();
 
 // middlewares
-app.use(morgan('dev')); // morgan en modo dev para ver las request en la consola
+app.use(morgan('combined')); // morgan en modo dev para ver las request en la consola
 app.use(express.json()); // para recibir req json
 app.use(express.urlencoded({ extended: false })); // para recibir req urlencoded
 app.use(cors()); // para que responda peticiones desde otros hosts
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname + '/../client/build')));
+app.use(express.static(path.join(__dirname + '/build')));
 
 // Handles any requests that don't match the ones above
-/* app.get('*', (req, res) =>{
-  res.sendFile(path.join(__dirname + '/../client/build/index.html'));
-}); */
-
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../client/build/index.html'));
-});
-
-app.get('/test', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../client/build/index.html'));
-});
-
-app.post('/login', (req, res) => {
-  console.log(req.body);
+app.get('*', (req, res) =>{
+  res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
 async function initApp({ port=3000 }) {
